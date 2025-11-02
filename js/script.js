4dc97fdfc8a79c8dbc471e64f86576e7df99943e
CandriPanjaitan <candri.informatika.08@gmail.com>
2025-11-02 07:25:46 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ setInterval(() => {
   current = (current + 1) % images.length;
   images[current].classList.add("active");
 }, 3000); // ganti gambar tiap 3 detik
+
+// hamburger
+const navbarMenu = document.querySelector(".navbar-menu");
+
+// klik hamburger
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarMenu.classList.toggle("active");
+};
+
+// klick di luar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarMenu.contains(e.target)){
+    navbarMenu.classList.remove('active');
+  }
+});
