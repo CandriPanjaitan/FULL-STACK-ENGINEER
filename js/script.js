88c69d21d27c64fd0cfe8c2722acbf35a7eb5e95
CandriPanjaitan <candri.informatika.08@gmail.com>
2025-11-04 10:20:56 +0700

    update fitur

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
 // feather
 feather.replace();
 
-// Carousel otomatis
-let current = 0;
-const images = document.querySelectorAll(".carousel img");
-
-setInterval(() => {
-  images[current].classList.remove("active");
-  current = (current + 1) % images.length;
-  images[current].classList.add("active");
-}, 3000); // ganti gambar tiap 3 detik
-
-// hamburger
-const navbarMenu = document.querySelector(".navbar-menu");
-
-// klik hamburger
+// Toogle Class Active
+const navMenu = document.querySelector(".nav-menu");
+// Ketika humberger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
-  navbarMenu.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
 
-// klick di luar
+// Klik di luar
 const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click', function(e) {
-  if(!hamburger.contains(e.target) && !navbarMenu.contains(e.target)){
-    navbarMenu.classList.remove('active');
+document.addEventListener('click', function(e){
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
   }
 });
+
